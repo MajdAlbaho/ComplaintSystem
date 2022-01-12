eef0ee1787cc25067c22675a0f8fac5bc64a5125
Majd Albaho <Majd.albaho@Gmail.com>
2022-01-12 09:53:07 +0200

    Update urls.py

--- complaint/urls.py
+++ complaint/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login_view, name='login'),
     path('login', views.login_view, name='login'),
     path('logged-out', views.logging_out_view, name='logout'),
     path('signup', views.sign_up_view, name='signup'),
